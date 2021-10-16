8cda09c7992f53dbfb747b5912f055f56a66a09f
Le Trung Kien <ltkienwork@gmail.com>
2021-10-17 04:37:07 +0700

    Update courses

--- AppDevGCD0805/Controllers/CoursesController.cs
+++ AppDevGCD0805/Controllers/CoursesController.cs
@@ -1,11 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using AppDevGCD0805.Data;
+using AppDevGCD0805.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppDevGCD0805.Controllers
 {
-    public class CoursesController
+    public class CoursesController : Controller
     {
+        private ApplicationDbContext _db;
+        public CoursesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public ActionResult Index()
+        {
+            var model = _db.Courses.Include(x => x.Category);
+            return View(model);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            var model = _db.Categories.ToList();
+            ViewBag.name = model;
+            return View(ViewBag.name);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Course course)
+        {
+
+            _db.Courses.Add(course);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var courseinDb = _db.Courses.SingleOrDefault(item => item.Id == Id);
+            _db.Courses.Remove(courseinDb);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+            var todoInDb = _db.Courses
+                .SingleOrDefault(t => t.Id == id);
+
+            return View(todoInDb);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Course course)
+        {
+            var courseinDb = _db.Courses.SingleOrDefault(item => item.Id == course.Id);
+            courseinDb.Name = course.Name;
+            courseinDb.Description = course.Description;
+            courseinDb.CategoryId = course.CategoryId;
+
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
